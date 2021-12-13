c4c5d4b4899f0c028ab78f08ade9ee29120825d7
FooFoomovich <95942457+FooFoomovich@users.noreply.github.com>
2021-12-13 10:09:49 -0600

    Create cart.test.js
    cart features cypress test.

---
+++ cypress/cart.test.js
@@ -0,0 +1,21 @@
+describe('cart', ()=> {
+  it('User should be able to navigate to cart.', ()=> {
+    
+    ...
+  })
+  
+  it('User should add item to cart which goes to cart.', ()=> {
+    
+    ...
+  })
+  
+  it('User can edit number of items for each cart entry.', ()=> {
+    
+    ...
+  })
+  
+  it('User can edit number of items for each cart entry.', ()=> {
+    
+    ...
+  })
+})
